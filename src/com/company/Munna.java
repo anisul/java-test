60d678cb10344f4fcdeb097cd34c7ccf8707046d
anisul <anisul.iut@gmail.com>
2020-01-06 01:01:13 +0100

    third commit

--- src/com/company/Munna.java
+++ src/com/company/Munna.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 public class Munna {
     public static void main(String[] args) {
         System.out.println("aaa");
+        System.out.println("aaa");
     }
 
 }
