69ae025f9f815824347668ca1893562181dc0b5a
anisul <anisul.iut@gmail.com>
2020-01-06 01:06:38 +0100

    ticket 101

--- src/com/company/Munna.java
+++ src/com/company/Munna.java
@@ -6,6 +6,8 @@ public class Munna {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("aaa");
+
+        System.out.println("ticket 101");
     }
 
 }
